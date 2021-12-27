209626ccaf7261a8f946cc8bd5d62a3142dd2968
Angelina <pretzel318@gmail.com>
2021-12-28 01:23:45 +0300

    folder refactoring

---
+++ js/navigation/navigation.js
@@ -0,0 +1,83 @@
+let field_container = document.querySelector(".field-container");
+let main_container = document.querySelector(".main-container");
+let menu_container =document.querySelector(".menu-container");
+
+let loading_block = document.querySelector(".loading__block");
+let starting_block = document.querySelector(".starting__block");
+
+let welcome_window = document.querySelector(".welcome__window");
+let game_rules_window = document.querySelector(".game-rules__window");
+let settings_window = document.querySelector(".settings__window");
+let win_window = document.querySelector(".win__window");
+
+// navigation buttons
+let btn1 = document.querySelector("#btn1");
+let btn2 = document.querySelector("#btn2");
+let btn3 = document.querySelector("#btn3");
+let btn4 = document.querySelector("#btn4");
+let btn5 = document.querySelector("#btn5");
+
+// settings buttons
+let btnSize_4 = document.querySelector(".size-4");
+let btnSize_6 = document.querySelector(".size-6");
+let btnEasy = document.querySelector(".easy-game");
+let btnHard = document.querySelector(".hard-game");
+
+// dropdown elements
+let menuBtn = document.querySelector(".menu");
+let menuItems = document.querySelector(".menu__items");
+
+btn1.addEventListener("click", showGameRules);
+
+btn2.addEventListener("click", showSettings);
+
+btn3.addEventListener("click", showSettings);
+
+btn4.addEventListener("click", function(){
+	showFieldAndStartGame(setting_size, setting_difficult);
+});
+
+btn5.addEventListener("click", showSettings);
+
+let setting_size = 4;
+let setting_difficult = 1;
+
+btnSize_4.addEventListener("click", function(){
+	setting_size = 4;
+	if (!$(this).hasClass("selected")) {
+		$(this).toggleClass("selected");
+		$(btnSize_6).toggleClass("selected");
+	}
+});
+
+btnSize_6.addEventListener("click", function(){
+	setting_size = 6;
+	if (!$(this).hasClass("selected")) {
+		$(this).toggleClass("selected");
+		$(btnSize_4).toggleClass("selected");
+	}
+});
+
+btnEasy.addEventListener("click", function(){
+	setting_difficult = 1;
+	if (!$(this).hasClass("selected")) {
+		$(this).toggleClass("selected");
+		$(btnHard).toggleClass("selected");
+	}
+});
+
+btnHard.addEventListener("click", function(){
+	setting_difficult = 2;
+	console.log("22")
+	if (!$(this).hasClass("selected")) {
+		$(this).toggleClass("selected");
+		$(btnEasy).toggleClass("selected");
+	}
+});
+
+
+
+menuBtn.addEventListener("click", function(){
+	$(menuBtn).toggleClass("active");
+	$(menuItems).toggleClass("active");
+});
---
+++ js/navigation_methods.js
@@ -0,0 +1,61 @@
+let isProgressStarted = false;
+
+function start() {
+	startProgress();
+}
+
+function hideLoadingWindow() {
+	$(loading_block).hide();
+	$(starting_block).show();
+	$(welcome_window).show();
+	$(menu_container).show();
+}
+
+function showFieldAndStartGame(sizeField, difficult) {
+	$(settings_window).hide();
+	$(main_container).hide();
+	$(field_container).show();
+
+	let settings = new Settings(sizeField, difficult);
+	let game = new Game(settings);
+	game.init();
+	game.run();
+}
+
+function showGameRules(){
+	$(welcome_window).hide();
+	$(game_rules_window).show();
+}
+
+function showSettings() {
+	$(game_rules_window).hide();
+	$(welcome_window).hide();
+	$(win_window).hide();
+	$(settings_window).show();
+}
+
+function showWinWindow(){
+	$(field_container).hide();
+	$(main_container).show();
+	$(win_window).show();
+}
+
+function startProgress() {
+	if (!isProgressStarted) {
+		isProgressStarted = true;
+		let elem = document.querySelector("#myBar");
+		let width = 1;
+
+		let frame = function () {
+			if (width >= 100) {
+				clearInterval(id);
+				isProgressStarted = false;
+				hideLoadingWindow();
+			} else {
+				width++;
+				elem.style.width = width + "%";
+			}
+		}
+		let id = setInterval(frame, 10);
+	}
+}
